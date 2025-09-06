dc63f5ea2f7c4ebd7348e1bd522a9074d4fb9661
taunyxlpz <taunyxlpz@gmail.com>
2025-09-05 20:33:40 -0400

    fix(upload): client page header order; dynamic=force-dynamic; revalidate=false; clean layout

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,6 +1,6 @@
 ﻿"use client";
 export const dynamic = "force-dynamic";
-export const revalidate = 0;
+export const revalidate = false;
 import { useEffect, useMemo, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 
@@ -88,4 +88,5 @@ export default function UploadPage() {
     </div>
   );
 }
+
 
